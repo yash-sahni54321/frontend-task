bbb96f45f20466a6b02eae6abf8ad547a275a0fd
yash-sahni54321 <yash.sahni54321@gmail..com>
2024-08-30 11:25:42 +0530

    UI fixup

--- src/assets/svgs/DocumentSVG.tsx
+++ src/assets/svgs/DocumentSVG.tsx
@@ -1,116 +1,97 @@
 import * as React from 'react';
 const DocumentSVG = (props: React.SVGProps<SVGElement>) => (
   <svg
+    xmlns='http://www.w3.org/2000/svg'
     width={60}
     height={63}
-    viewBox='0 0 60 63'
     fill='none'
-    xmlns='http://www.w3.org/2000/svg'
     {...props}
   >
-    <g filter='url(#filter0_d_63_70)'>
+    <g filter='url(#a)'>
       <rect
-        x={12.1338}
-        y={6.77997}
-        width={35.7333}
-        height={37.8658}
-        rx={13.3333}
-        fill='url(#paint0_linear_63_70)'
+        width={35.733}
+        height={37.866}
+        x={12.134}
+        y={6.78}
+        fill='url(#b)'
+        rx={13.333}
       />
       <rect
-        x={12.9338}
-        y={7.57997}
-        width={34.1333}
-        height={36.2658}
-        rx={12.5333}
-        stroke='white'
+        width={34.133}
+        height={36.266}
+        x={12.934}
+        y={7.58}
+        stroke='#fff'
         strokeOpacity={0.3}
         strokeWidth={1.6}
+        rx={12.533}
       />
     </g>
-    <g filter='url(#filter1_d_63_70)'>
+    <g filter='url(#c)'>
       <path
-        d='M32.4997 22.6667H36.1663L31.6663 18.1667V21.8333C31.6663 22.3333 31.9997 22.6667 32.4997 22.6667ZM32.4997 24.3333C31.083 24.3333 29.9997 23.25 29.9997 21.8333V17.6667H25.833C24.4163 17.6667 23.333 18.75 23.333 20.1667V31.8333C23.333 33.25 24.4163 34.3333 25.833 34.3333H34.1663C35.583 34.3333 36.6663 33.25 36.6663 31.8333V24.3333H32.4997ZM27.4997 22.6667H28.333C28.833 22.6667 29.1663 23 29.1663 23.5C29.1663 24 28.833 24.3333 28.333 24.3333H27.4997C26.9997 24.3333 26.6663 24 26.6663 23.5C26.6663 23 26.9997 22.6667 27.4997 22.6667ZM32.4997 31H27.4997C26.9997 31 26.6663 30.6667 26.6663 30.1667C26.6663 29.6667 26.9997 29.3333 27.4997 29.3333H32.4997C32.9997 29.3333 33.333 29.6667 33.333 30.1667C33.333 30.6667 32.9997 31 32.4997 31ZM32.4997 27.6667H27.4997C26.9997 27.6667 26.6663 27.3333 26.6663 26.8333C26.6663 26.3333 26.9997 26 27.4997 26H32.4997C32.9997 26 33.333 26.3333 33.333 26.8333C33.333 27.3333 32.9997 27.6667 32.4997 27.6667Z'
-        fill='white'
+        fill='#fff'
+        d='M32.5 22.667h3.666l-4.5-4.5v3.666c0 .5.334.834.834.834Zm0 1.666c-1.417 0-2.5-1.083-2.5-2.5v-4.166h-4.167c-1.417 0-2.5 1.083-2.5 2.5v11.666c0 1.417 1.083 2.5 2.5 2.5h8.333c1.417 0 2.5-1.083 2.5-2.5v-7.5H32.5Zm-5-1.666h.833c.5 0 .833.333.833.833 0 .5-.333.833-.833.833H27.5c-.5 0-.834-.333-.834-.833 0-.5.334-.833.834-.833Zm5 8.333h-5c-.5 0-.834-.333-.834-.833 0-.5.334-.834.834-.834h5c.5 0 .833.334.833.834 0 .5-.333.833-.833.833Zm0-3.333h-5c-.5 0-.834-.334-.834-.834 0-.5.334-.833.834-.833h5c.5 0 .833.333.833.833 0 .5-.333.834-.833.834Z'
       />
     </g>
     <defs>
       <filter
-        id='filter0_d_63_70'
-        x={0.400457}
-        y={0.913302}
+        id='a'
         width={59.2}
-        height={61.3325}
-        filterUnits='userSpaceOnUse'
+        height={61.333}
+        x={0.4}
+        y={0.913}
         colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
       >
         <feFlood floodOpacity={0} result='BackgroundImageFix' />
         <feColorMatrix
           in='SourceAlpha'
-          type='matrix'
-          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
           result='hardAlpha'
+          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
         />
-        <feOffset dy={5.86667} />
-        <feGaussianBlur stdDeviation={5.86667} />
+        <feOffset dy={5.867} />
+        <feGaussianBlur stdDeviation={5.867} />
         <feComposite in2='hardAlpha' operator='out' />
-        <feColorMatrix
-          type='matrix'
-          values='0 0 0 0 0.219608 0 0 0 0 0.619608 0 0 0 0 0.647059 0 0 0 0.4 0'
-        />
+        <feColorMatrix values='0 0 0 0 0.219608 0 0 0 0 0.619608 0 0 0 0 0.647059 0 0 0 0.4 0' />
+        <feBlend in2='BackgroundImageFix' result='effect1_dropShadow_63_70' />
         <feBlend
-          mode='normal'
-          in2='BackgroundImageFix'
-          result='effect1_dropShadow_63_70'
-        />
-        <feBlend
-          mode='normal'
           in='SourceGraphic'
           in2='effect1_dropShadow_63_70'
           result='shape'
         />
       </filter>
       <filter
-        id='filter1_d_63_70'
-        x={22.7997}
-        y={17.6667}
+        id='c'
         width={14.4}
-        height={17.7333}
-        filterUnits='userSpaceOnUse'
+        height={17.733}
+        x={22.8}
+        y={17.667}
         colorInterpolationFilters='sRGB'
+        filterUnits='userSpaceOnUse'
       >
         <feFlood floodOpacity={0} result='BackgroundImageFix' />
         <feColorMatrix
           in='SourceAlpha'
-          type='matrix'
-          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
           result='hardAlpha'
+          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
         />
-        <feOffset dy={0.533333} />
-        <feGaussianBlur stdDeviation={0.266667} />
+        <feOffset dy={0.533} />
+        <feGaussianBlur stdDeviation={0.267} />
         <feComposite in2='hardAlpha' operator='out' />
-        <feColorMatrix
-          type='matrix'
-          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0'
-        />
+        <feColorMatrix values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0' />
+        <feBlend in2='BackgroundImageFix' result='effect1_dropShadow_63_70' />
         <feBlend
-          mode='normal'
-          in2='BackgroundImageFix'
-          result='effect1_dropShadow_63_70'
-        />
-        <feBlend
-          mode='normal'
           in='SourceGraphic'
           in2='effect1_dropShadow_63_70'
           result='shape'
         />
       </filter>
       <linearGradient
-        id='paint0_linear_63_70'
-        x1={30.0005}
-        y1={6.77997}
-        x2={30.0005}
-        y2={44.6458}
+        id='b'
+        x1={30}
+        x2={30}
+        y1={6.78}
+        y2={44.646}
         gradientUnits='userSpaceOnUse'
       >
         <stop stopColor='#70F2DB' />
--- src/components/BottomNagivation/BottomNavigation.tsx
+++ src/components/BottomNagivation/BottomNavigation.tsx
@@ -1,8 +1,10 @@
 import GlobeSVG from '@/assets/svgs/GlobeSVG';
 import MagicSVG from '@/assets/svgs/MagicSVG';
-import React from 'react';
+import React, { useState } from 'react';
 
 const BottomNavigation = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
   const navLinks1 = [
     'Home',
     'Feature',
@@ -11,32 +13,62 @@ const BottomNavigation = () => {
     'Testimonials',
   ];
 
+  const handleClick = index => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className='w-full flex items-center justify-center mt-8 md:mt-32'>
-      <div className='px-4 py-2 bg-white rounded-3xl w-full md:w-2/3 flex flex-col md:flex-row justify-between items-center'>
-        <div className='flex gap-x-2 md:gap-x-4 flex-wrap justify-center items-center mb-4 md:mb-0'>
+      <div className='px-4 py-2 bg-white rounded-3xl w-full md:w-4/5 flex flex-col md:flex-row justify-between items-center'>
+        <div className='flex gap-x-2 md:gap-x-2 flex-wrap justify-center items-center'>
           {navLinks1.map((link, index) => (
             <button
               key={index}
-              className='text-black hover:text-gray-700 text-sm'
+              className={`text-sm px-4 py-2 rounded-md ${
+                activeIndex === index
+                  ? 'bg-gradient-to-b from-[#E95B4A] to-[#B164C4] text-white'
+                  : 'text-black hover:text-gray-700'
+              }`}
+              onClick={() => handleClick(index)}
             >
               {link}
             </button>
           ))}
           <MagicSVG className='mt-2 md:mt-0' />
-          <button className='text-black hover:text-gray-700 text-sm'>
+          <button
+            className={`text-sm px-4 py-2 rounded-md ${
+              activeIndex === navLinks1.length
+                ? 'bg-gradient-to-b from-[#E95B4A] to-[#B164C4] text-white'
+                : 'text-black hover:text-gray-700'
+            }`}
+            onClick={() => handleClick(navLinks1.length)}
+          >
             FAQ's
           </button>
-          <button className='text-black hover:text-gray-700 text-sm'>
+          <button
+            className={`text-sm px-4 py-2 rounded-md ${
+              activeIndex === navLinks1.length + 1
+                ? 'bg-gradient-to-b from-[#E95B4A] to-[#B164C4] text-white'
+                : 'text-black hover:text-gray-700'
+            }`}
+            onClick={() => handleClick(navLinks1.length + 1)}
+          >
             Blogs
           </button>
         </div>
-        <div className='flex gap-x-4 flex-wrap justify-center items-center'>
+        <div className='flex gap-x-2 flex-wrap justify-center items-center ml-2'>
           <GlobeSVG width={30} height={30} />
-          <button className='text-black hover:text-gray-700 text-sm'>
+          <button
+            className={`text-sm px-4 py-2 rounded-md ${
+              activeIndex === navLinks1.length + 2
+                ? 'bg-gradient-to-b from-[#E95B4A] to-[#B164C4] text-white'
+                : 'text-black hover:text-gray-700'
+            }`}
+            onClick={() => handleClick(navLinks1.length + 2)}
+          >
             Sign in
           </button>
-          <button className='text-black hover:text-gray-700 text-sm'>
+          <button className='text-sm px-4 py-2 rounded-md bg-gradient-to-b from-[#E95B4A] to-[#B164C4] text-white'>
             Live demo
           </button>
         </div>
