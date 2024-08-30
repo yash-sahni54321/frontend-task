36fb4c3aaf9abe5cd4c1c4322c809ca8fb102407
yash-sahni54321 <yash.sahni54321@gmail..com>
2024-08-30 05:37:35 +0530

    Added Navigation and sidebar.

--- src/components/HomePlateformSelect/DragAndDropBox.tsx
+++ src/components/HomePlateformSelect/DragAndDropBox.tsx
@@ -7,17 +7,16 @@ const DragAndDrop = () => {
     <Droppable droppableId='Icons'>
       {(provided, snapshot) => (
         <div
-          className={`${snapshot.isDraggingOver ? 'dragactive' : ''} w-full  max-w-md `}
+          className={`${snapshot.isDraggingOver ? 'dragactive' : ''} w-full max-w-md`}
           ref={provided.innerRef}
           {...provided.droppableProps}
         >
           <div
-            className={` h-32 bg-white rounded-3xl border border-dashed border-gray-300 flex items-center justify-center `}
+            className='h-32 bg-white rounded-3xl border border-dashed border-gray-300 flex items-center justify-center'
             style={{ boxShadow: '0 22px 60px rgba(0, 0, 0, 0.15)' }}
           >
             <div className='flex flex-col justify-center items-center'>
               <div className='animate-spin'>
-                {' '}
                 <EllipseSVG width={50} height={50} />
               </div>
               <p className='text-gray-600 text-lg font-medium mt-2'>
